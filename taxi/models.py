ff1dc6c88c756714cb380ec0d0cdab083f2946cf
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-11-30 04:54:52 +0200

    Deleted unused imports

--- taxi/models.py
+++ taxi/models.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django_countries.fields import CountryField
-
 
 
 class Manufacturer(models.Model):
